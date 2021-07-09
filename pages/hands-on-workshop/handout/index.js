78eea2a7e2e8cc48a451c8b8ca6647f004be194d
Irina Illustrova <illustrova@gmail.com>
2021-07-09 11:34:45 +0300

    meta for handout page

--- pages/hands-on-workshop/handout/index.js
+++ pages/hands-on-workshop/handout/index.js
@@ -3,8 +3,10 @@ import React from "react";
 import { Text, Card, Badge, Emoji } from "@goright/design-system";
 
 import Link from "@components/link";
-import Head from "next/head";
 import { useRouter } from "next/router";
+import { NextSeo } from "next-seo";
+
+
 export const blocks = [
   {
     title: "Workshop flow",
@@ -144,38 +146,28 @@ const Blocks = (props) => {
 export default function HandoutPage() {
   return (
     <>
-      <Head>
-        <title>Hands-on with design systems workshop by GoRight</title>
-        <meta
-          name="Description"
-          content="This workshop helps you learn and practice design systems working in a team. You can join as a designer or developer."
-        />
-        {/* OG tags */}
-        <meta
-          property="og:title"
-          content="Hands-on with Design Systems: 2-day workshop with Figma & React"
-        />
-        <meta
-          property="og:url"
-          content="https://hands-on-workshop.goright.io"
-        />
-        <meta property="og:image" content="/poster.png" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:description"
-          content="This workshop helps you learn and practice design systems working in a team. You can join as a designer or developer."
-        />
-        <meta property="og:locale" content="en_GB" />
-        <meta
-          name="description"
-          content="Hands-on with Design Systems: 2-day workshop with Figma & React. This workshop helps you learn and practice design systems working in a team. You can join as a designer or developer."
-        />
-        <meta
-          name="keywords"
-          content="design systems, workshop, team work, ReactJS, Figma, styled-components, Storybook, design, frontend, development"
-        />
-        <meta name="robots" content="noindex, nofollow" />
-      </Head>
+     <NextSeo
+        noindex={true}
+        nofollow={true}
+        title="Handout materials for Hands-on with Design Systems Workshop"
+        description="All the necessary documentation for the participants of the workshop"
+        keywords="design systems, workshop, team work, ReactJS, Figma, styled-components, Storybook, design, frontend, development"
+        canonical={canonical}
+        openGraph={{
+          type: "website",
+          locale: "en_IE",
+          url: "https://hands-on-workshop.goright.io/handout",
+          site_name: "GoRight.io",
+          images: [
+            {
+              url: baseUrl + "/poster.png",
+              width: 800,
+              height: 600,
+              alt: "Hands-on Workshop poster",
+            },
+          ]
+        }}
+    />
 
       <div className="container max-w-5xl mx-auto mt-16">
         <div className="text-center text-light-on-background-900">
