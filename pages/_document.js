ab03ce523e2a591d78b68d6778c96a1f201875e6
Irina Illustrova <illustrova@gmail.com>
2021-08-13 15:20:17 +0300

    update favicon

--- pages/_document.js
+++ pages/_document.js
@@ -50,6 +50,37 @@ class MyDocument extends Document {
           <script
             src={`https://www.googleoptimize.com/optimize.js?id=${OPT_CONTAINER_ID}`}
           ></script>
+          {/* Favicon */}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/static/safari-pinned-tab.svg"
+            color="#0047ff"
+          />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#0047ff"></meta>
+          <meta
+            name="msapplication-config"
+            content="/static/browserconfig.xml"
+          ></meta>
+          <meta name="theme-color" content="#ffffff"></meta>
         </Head>
         <body className="">
           <Main />
